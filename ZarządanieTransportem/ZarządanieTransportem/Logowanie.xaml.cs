2431c88e0af3c05266d7f9b377abd16ff960495a
dawids162 <dawids162@gmail.com>
2015-03-19 17:50:13 +0100

    PierwszyWpis

---
+++ ZarządanieTransportem/ZarządanieTransportem/Logowanie.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZarządanieTransportem
+{
+    /// <summary>
+    /// Interaction logic for Logowanie.xaml
+    /// </summary>
+    public partial class Logowanie : Window
+    {
+        MainWindow form1;
+        public Logowanie(MainWindow form1)
+        {
+            this.form1 = form1;
+            InitializeComponent();
+        }
+
+        private void ButtonLogowanie_Click(object sender, RoutedEventArgs e)
+        {
+            if(LoginText.Text=="test" && HasloText.Text=="test")
+            {
+                this.form1.login = "test";
+                this.form1.haslo = "test";
+                this.Close();
+            }
+        }
+    }
+}
---
+++ ZarządanieTransportem/ZarządanieTransportem/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZarządanieTransportem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string login, haslo;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Logowanie Logowanie1 = new Logowanie(this);
+           // Logowanie1.Owner = this;
+            Logowanie1.ShowDialog();
+           if(login=="test" && login == "test")
+           {
+               Mapa.Navigate("https://mapa.targeo.pl");
+               odblokujWszystko();
+           }
+            
+            
+        }
+        private void odblokujWszystko()
+        {
+            TreePracownicy.IsEnabled = true;
+            TreeSrodkitrwale.IsEnabled = true;
+            TreeUslugiTransportowe.IsEnabled = true;
+            TreePaliwo.IsEnabled = true;
+            GridStatus.IsEnabled = true;
+            Mapa.IsEnabled = true;
+        }
+    }
+}
